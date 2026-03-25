8507ab20da0742bbdf83660e4468e54600d4e8dc
CiciTang090905 <wtang28@jh.edu>
2026-03-25 09:18:53 -0400

    counter-04: Create counter layout with JSX

--- counter-react/src/App.jsx
+++ counter-react/src/App.jsx
@@ -1,7 +1,24 @@
+import "./App.css";
+
 export default function App() {
+  const count = 0;
+
   return (
-    <div>
-      <h1>Hello, World!</h1>
+    <div className="counter">
+      <h1>Count: {count}</h1>
+      <button>+</button>
+      <button>&minus;</button>
+      <button>Reset</button>
     </div>
   );
 }
+
+//The HTML-like syntax inside the return statement is called JSX (JavaScript XML)
+// JSX gets transformed into React.createElement() calls behind the scenes
+//<h1>Hello, World!</h1>
+// becomes
+// React.createElement('h1', null, 'Hello, World!')
+//JSX gives you syntax highlighting, auto-completion, and compile-time error checking.
+
+
+//2 differences: className instead of class, and no id attributes, we attach event handlers directly to the buttons instead of using document.getElementById() to select them.
